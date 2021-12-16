c35aac3ae5f8dce3f564a3f0e93374a031c1f321
marianeacsu <58784210+marianeacsu@users.noreply.github.com>
2021-12-16 12:30:39 +0200

    update code phase 2

--- Code/snakemenu.h
+++ Code/snakemenu.h
@@ -2,6 +2,7 @@
 #define SNAKEMENU_H
 #include <LiquidCrystal.h>
 
+int verif = 0;
 
 class Menu{
  /* private:
@@ -20,6 +21,28 @@ class Menu{
        lcd.setCursor(0, 1);
        lcd.print("Play SNAKE!");
        delay(6000);
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       lcd.print("Author: ");
+       lcd.setCursor(0, 1);
+       lcd.print("Neacsu Maria");
+       delay(4000);
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       lcd.print("Github user :");
+       lcd.setCursor(0,1);
+       lcd.print("marianeacsu");
+       delay(3000);
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       lcd.print("Repo :");
+       lcd.setCursor(0,1);
+       lcd.print("Snake");
+       delay(3000);
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       lcd.print("Level 1:");
+       delay(3000);
        lcd.clear();
     }
     
