297c964b817cdee3552f17a5c2cf389c99bd3f3a
marianeacsu <58784210+marianeacsu@users.noreply.github.com>
2021-12-21 19:09:28 +0200

    Update snakemenu.h

--- Code/snakemenu.h
+++ Code/snakemenu.h
@@ -1,53 +1,53 @@
-#ifndef SNAKEMENU_H  
+#ifndef SNAKEMENU_H
 #define SNAKEMENU_H
 #include <LiquidCrystal.h>
 
 int verif = 0;
 
-class Menu{
-  
+class Menu {
+
   public:
 
-    void startgame(LiquidCrystal &lcd){
-       lcd.setCursor(0, 0);
-       lcd.print("Don't be a SNAKE");
-       lcd.setCursor(0, 1);
-       lcd.print("Play SNAKE!");
-       delay(6000);
-       lcd.clear();
-       lcd.setCursor(0, 0);
-       lcd.print("Author: ");
-       lcd.setCursor(0, 1);
-       lcd.print("Neacsu Maria");
-       delay(4000);
-       lcd.clear();
-       lcd.setCursor(0, 0);
-       lcd.print("Github user :");
-       lcd.setCursor(0,1);
-       lcd.print("marianeacsu");
-       delay(3000);
-       lcd.clear();
-       lcd.setCursor(0, 0);
-       lcd.print("Repo :");
-       lcd.setCursor(0,1);
-       lcd.print("Snake");
-       delay(3000);
-       lcd.clear();
-       lcd.setCursor(0, 0);
-       lcd.print("Level 1:");
-       delay(3000);
-       lcd.clear();
-    }
-    
-    void endgame(LiquidCrystal &lcd){
+    void startgame(LiquidCrystal &lcd) {
+      lcd.setCursor(0, 0);
+      lcd.print("Don't be a SNAKE");
+      lcd.setCursor(0, 1);
+      lcd.print("Play SNAKE!");
+      delay(6000);
       lcd.clear();
-      lcd.setCursor(0,0);
+      lcd.setCursor(0, 0);
+      lcd.print("Author: ");
+      lcd.setCursor(0, 1);
+      lcd.print("Neacsu Maria");
+      delay(4000);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Github user :");
+      lcd.setCursor(0, 1);
+      lcd.print("marianeacsu");
+      delay(3000);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Repo :");
+      lcd.setCursor(0, 1);
+      lcd.print("Snake");
+      delay(3000);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Level 1:");
+      delay(3000);
+      lcd.clear();
+    }
+
+    void endgame(LiquidCrystal &lcd) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
       lcd.print("Try again? :)");
       delay(2000);
       lcd.clear();
     }
 
-    
-}snakeMenuObject;
+
+} snakeMenuObject;
 
 #endif
